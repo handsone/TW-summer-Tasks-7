f0523691846d4bb7c971dd70f58660f4d6b4b759
gregrory <gregrory>
2017-08-23 15:20:19 +0800

    更改了任务七

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -10,27 +10,22 @@ var main = require("../lib/main.js");
 
 
 describe("测试描述", function(){
-    sinon.spy(console, 'log');
+	sinon.spy(console, 'log');
 
-    it("It should contain '99 bottles of beer on the wall, 99 bottles of beer.\nTake on down and pass it around, 98 bottles of beer.\n'", function(){
-
-        var result = main();
-        var expect_string = '99 bottles of beer on the wall, 99 bottles of beer.\nTake one down and pass it around, 98 bottles of beer.\n';
-        
-        expect(result).to.contain(expect_string);
-    });
-    it("It should contain '99 bottles of beer on the wall, 99 bottles of beer.\nTake on down and pass it around, 98 bottles of beer.\n (for loop 97 times)'", function(){
-
-        var result = main();
-        var expect_string = '99 bottles of beer on the wall, 99 bottles of beer.\nTake one down and pass it around, 98 bottles of beer.\n98 bottles of beer on the wall, 98 bottles of beer.\nTake one down and pass it around, 97 bottles of beer.\n' ;
-        
-        expect(result).to.contain(expect_string);
-    });
-    it("It should contain '99 bottles of beer on the wall, 99 bottles of beer.\nTake on down and pass it around, 98 bottles of beer.\n (for loop 97 times) and add '2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, no more bottles of beer.\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles fo beer on the wall.", function(){
-
-        var result = main();
-        var expect_string = '2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer.\n1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no more bottles of beer.\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles fo beer on the wall.';
-        
-        expect(result).to.contain(expect_string);
-    });
+	it('When input 0 , then output "No more bottles of beer on the wall, no more bottles of beer.Go to the store and buy some more, 99 bottles of beer on the wall."', () => {
+		let result = main(0);
+		expect(result).to.be.equal("No more bottles of beer on the wall, no more bottles of beer.Go to the store and buy some more, 99 bottles of beer on the wall.")	;
+	});
+	it('When input 1 , then output "1 bottle of beer on the wall, 1 bottle of beer.Take one down and pass it around, no more bottles of beer on the wall."', () => {
+		let result = main(1);
+		expect(result).to.be.equal("1 bottle of beer on the wall, 1 bottle of beer.Take one down and pass it around, no more bottles of beer on the wall.");
+	});
+	it('When input 2 , then output"2 bottles of beer on the wall, 2 bottles of beer.Take one down and pass it around, 1 bottle of beer on the wall."', () => {
+		let result = main(2);
+		expect(result).to.be.equal("2 bottles of beer on the wall, 2 bottles of beer.  Take one down and pass it around, 1 bottle of beer on the wall.");
+	});
+	it('When input 66 , then output"66 bottles of beer on the wall, 66 bottles of beer.Take one down and pass it around, 64 bottle of beer on the wall."', () => {
+		let result = main(66);
+		expect(result).to.be.equal("66 bottles of beer on the wall, 66 bottles of beer.  Take one down and pass it around, 65 bottle of beer on the wall.");
+	});
 });
